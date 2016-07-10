e9af55e63c25d90d703128f29ee45a6cadc93b03
FernandoGabane <fernandogabane@gmail.com>
2016-07-10 13:36:13 -0300

    Update GitHub.java

--- src/GitHub/GitHub.java
+++ src/GitHub/GitHub.java
@@ -1,11 +1,6 @@
 package GitHub;
 
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+
 
 public class GitHub {
 
